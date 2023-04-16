f86e102a93c4779fcb3cbeb480b4086fe520f9f1
Faical OUDBIB <faical.oudbib@ext.euronews.com>
2023-04-16 10:44:01 +0200

    Calling bingo numbers scenario

--- index.ts
+++ index.ts
@@ -1,3 +1,27 @@
-export function func(): void {
-    // @TODOFO: implement.
+
+export function callBingoNumbers(suite: number[]): number {
+
+    return suite.length ? Number(suite.pop()) : 0
+}
+
+export function numbersArray(): number[] {
+    return Array.from({ length: 75 }, (_, index) => index + 1)
+}
+
+export function scramble(array: number[]): number[] {
+    const clone = Array.from(array)
+    // Scrambling action
+    for (let index = 0; index < clone.length; index++) {
+        const newPosition = randomPosition()
+        const tmp = clone[index]
+        clone[index] = clone[newPosition]
+        clone[newPosition] = tmp
+    }
+
+    return clone
+}
+
+export function randomPosition(): number {
+
+    return Math.floor(Math.random() * 75)
 }
--- test.spec.ts
+++ test.spec.ts
@@ -1,8 +1,57 @@
-import { expect, test } from 'vitest'
+import { describe, expect, test } from 'vitest'
 
-import { multiplicationTable } from '.'
+import { callBingoNumbers, numbersArray, scramble } from '.'
 
+function randomnessTester(suite: number[]): number {
+    const calledBingoNumber: number = callBingoNumbers(suite)
 
-test('Should __name_test', () => {
-    expect(multiplicationTable()).toEqual([[]])
+    expect(calledBingoNumber).toBeLessThanOrEqual(75)
+    expect(calledBingoNumber).toBeGreaterThanOrEqual(1)
+
+    return calledBingoNumber
+}
+
+// Given I have a Bingo caller
+// When I call a number
+// Then the number is between 1 and 75 inclusive ✅
+describe('Given I have a bingo caller', () => {
+    describe('When I call a number', () => {
+        test('Then the number is between 1 and 75 inclusive', () => {
+            const suite = numbersArray()
+
+            for (let index = 0; index < 75; index++) {
+                randomnessTester(suite)
+            }
+        })
+    })
+})
+
+describe('scramble', () => {
+    test('works fine', () => {
+        const entry = numbersArray()
+        const result = numbersArray()
+
+        expect(scramble(entry).length).toEqual(result.length)
+        expect(scramble(entry).sort()).toEqual(result.sort())
+    })
+})
+
+// Given I have a Bingo caller
+// When I call a number 75 times
+// Then all numbers between 1 and 75 are present ✅
+// And no number has been called more than once ✅
+describe('Given I have a bingo caller', () => {
+    describe('When I call a number 75 times', () => {
+        test('Then all numbers between 1 and 75 are present', () => {
+            const suite = numbersArray()
+            const allCalledNumbers: number[] = []
+            const expected = numbersArray().sort()
+
+            for (let index = 1; index <= 75; index++) {
+                allCalledNumbers.push(randomnessTester(suite))
+            }
+
+            expect(allCalledNumbers.sort()).toEqual(expected)
+        })
+    })
 })
