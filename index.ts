3d8479e885906fa7535c6f2e1bcf7c8966d8d422
Faical OUDBIB <faical.oudbib@ext.euronews.com>
2023-04-21 20:52:22 +0200

    find-missing-letter: a lengthy solution

--- index.ts
+++ index.ts
@@ -1,3 +1,55 @@
-export function func(): void {
-    // @TODOFO: implement.
+// https://www.codewars.com/kata/5839edaa6754d6fec10000a2
+// I guess a somewhat lengthy solution to this Kata, lol.
+
+export function findMissingLetter(array: string[]): string {
+    if (array.length === 2) {
+        return twoLettersFinder(array)
+    }
+
+    // Somehow, generate all sequential two-letter's array from your four-letter's array.
+    const subArray = setOfTwoLettersArrays(array)
+
+    for (let twoLettersArray of subArray) {
+
+        if (lettersDistance(twoLettersArray) !== 1) {
+
+            return twoLettersFinder(twoLettersArray)
+        }
+    }
+
+    return ''
+}
+
+function twoLettersFinder(array: string[]): string {
+    const firstLetter = array[0].toLocaleLowerCase()
+
+    if (firstLetter === 'z') {
+        return 'a'
+    }
+
+    const followingLetter = firstLetter.charCodeAt(0) + 1
+    const stringifiedLetter = String.fromCharCode(followingLetter)
+
+    return formatResult(array[0], stringifiedLetter)
+}
+
+function lettersDistance(array: string[]): number {
+
+    return array[1].charCodeAt(0) - array[0].charCodeAt(0)
+}
+
+export function setOfTwoLettersArrays(array: string[]): string[][] {
+    const twoLettersArrays: string[][] = []
+
+    for (let index = 0; index < array.length - 1; index++) {
+        twoLettersArrays.push([array[index], array[index + 1]])
+    }
+
+    return twoLettersArrays
+}
+
+function formatResult(benchmark: string, letter: string): string {
+    const isUpperCase = benchmark.toUpperCase() === benchmark
+
+    return isUpperCase ? letter.toUpperCase() : letter
 }
--- test.spec.ts
+++ test.spec.ts
@@ -1,8 +1,125 @@
-import { expect, test } from 'vitest'
+import { expect, test, describe } from 'vitest'
 
-import { func } from '.'
+import { findMissingLetter, setOfTwoLettersArrays } from '.'
 
+describe('Two letters arrays', () => {
+    test('Should return "b" for array ["a", "c"]', () => {
+        const array = ['a', 'c']
+        const missingLetter = 'b'
 
-test('Should__name__test', () => {
-    expect(func()).toEqual([[]])
+        expect(findMissingLetter(array)).toEqual(missingLetter)
+    })
+
+    test('Should return "y" for array ["x", "z"]', () => {
+        const array = ['x', 'z']
+        const missingLetter = 'y'
+
+        expect(findMissingLetter(array)).toEqual(missingLetter)
+    })
+
+    test('Should return "o" for array ["n", "p"]', () => {
+        const array = ['n', 'p']
+        const missingLetter = 'o'
+
+        expect(findMissingLetter(array)).toEqual(missingLetter)
+    })
+})
+
+describe('Three letters arrays', () => {
+    test('Should return "o" for array ["n", "p", "q"]', () => {
+        const array = ['n', 'p', 'q']
+        const missingLetter = 'o'
+
+        expect(findMissingLetter(array)).toEqual(missingLetter)
+    })
+
+    test('Should return "p" for array ["n", "o", "q"]', () => {
+        const array = ['n', 'o', 'q']
+        const missingLetter = 'p'
+
+        expect(findMissingLetter(array)).toEqual(missingLetter)
+    })
+
+    test('Should return "p" for array ["a", "b", "d"]', () => {
+        const array = ['a', 'b', 'd']
+        const missingLetter = 'c'
+
+        expect(findMissingLetter(array)).toEqual(missingLetter)
+    })
+
+    test('Should return "p" for array ["a", "c", "d"]', () => {
+        const array = ['a', 'c', 'd']
+        const missingLetter = 'b'
+
+        expect(findMissingLetter(array)).toEqual(missingLetter)
+    })
+})
+
+describe('setOfTwoLettersArrays', () => {
+    test('Should return two arrays from a three-letter\'s array', () => {
+        const threeLettersArray = ['a', 'b', 'c']
+
+        expect(setOfTwoLettersArrays(threeLettersArray).length).toBe(2)
+    })
+
+    test('Should return the two arrays ["a", "b"] and ["b", "c"] from a three-letter\'s array ["a", "b", "c"]', () => {
+        const threeLettersArray = ['a', 'b', 'c']
+        const result = [['a', 'b'], ['b', 'c']]
+
+        expect(setOfTwoLettersArrays(threeLettersArray)).toEqual(result)
+    })
+
+    test('Should return the two arrays ["x", "y"] and ["y", "z"] from a three-letter\'s array ["x", "y", "z"]', () => {
+        const threeLettersArray = ['x', 'y', 'z']
+        const result = [['x', 'y'], ['y', 'z']]
+
+        expect(setOfTwoLettersArrays(threeLettersArray)).toEqual(result)
+    })
+
+    test('Should return the two arrays ["a", "b"], ["b", "c"] and ["c", "d"] from a three-letter\'s array ["a", "b", "c", "d"]', () => {
+        const threeLettersArray = ['a', 'b', 'c', 'd']
+        const result = [['a', 'b'], ['b', 'c'], ['c', 'd']]
+
+        expect(setOfTwoLettersArrays(threeLettersArray)).toEqual(result)
+    })
+
+    test('Should return the two arrays ["v", "w"], ["w", "x"], ["x", "y"] and ["y", "z"] from a three-letter\'s array ["v", "w", "x", "y", "z"]', () => {
+        const threeLettersArray = ['v', 'w', 'x', 'y', 'z']
+        const result = [['v', 'w'], ['w', 'x'], ['x', 'y'], ['y', 'z']]
+
+        expect(setOfTwoLettersArrays(threeLettersArray)).toEqual(result)
+    })
+})
+
+describe('Four letters arrays', () => {
+    test('Should return "o" for array ["n", "p", "q", "r"]', () => {
+        const array = ['n', 'p', 'q', 'r']
+        const missingLetter = 'o'
+
+        expect(findMissingLetter(array)).toEqual(missingLetter)
+    })
+})
+
+describe('Five letters arrays', () => {
+    test('Should return "o" for array ["n", "p", "q", "r", "s"]', () => {
+        const array = ['n', 'p', 'q', 'r', 's']
+        const missingLetter = 'o'
+
+        expect(findMissingLetter(array)).toEqual(missingLetter)
+    })
+})
+
+describe('Five letters arrays with UPPER CASE letters', () => {
+    test('Should return "O" for array ["N", "P", "Q", "R", "S"]', () => {
+        const array = ['N', 'P', 'Q', 'R', 'S']
+        const missingLetter = 'O'
+
+        expect(findMissingLetter(array)).toEqual(missingLetter)
+    })
+})
+
+test.skip('Should__name__test', () => {
+    const array = ['']
+
+    expect(findMissingLetter(array)).toEqual([[]])
 })
